f6b95d8b8f6077d6581ff9d4ebc87e07a2413bce
chamila senaratne <chamilasenaratne04@gmail.com>
2025-03-02 01:15:10 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
-import HomePage from "./pages/HomePage";
 
 function App() {
   return(
