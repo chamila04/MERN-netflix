299b166aeda79907fe5020a691de77e66f1197fc
chamila senaratne <chamilasenaratne04@gmail.com>
2025-03-12 00:31:17 +0530

    scroll

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useContentStore } from "../store/content";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { SMALL_IMG_BASE_URL } from "../utils/constants";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const MovieSlider = ({ category }) => {
   const { contentType } = useContentStore();
   const [content, setContent] = useState([]);
+  const [showArrows, setShowArrows] = useState(false);
+
+  const sliderRef = useRef(null);
 
   const formattedCategoryName =
     category.replaceAll("_", " ")[0].toUpperCase() +
@@ -24,13 +28,28 @@ const MovieSlider = ({ category }) => {
     getContent();
   }, [contentType, category]);
 
+  const scrollLeft = () => {
+    if(sliderRef.current) {
+        sliderRef.current.scrollBy({left:-sliderRef.current.offsetWidth, behavior: 'smooth'})
+    }
+  };
+  const scrollRight = () => {
+    if(sliderRef.current) {
+        sliderRef.current.scrollBy({left:sliderRef.current.offsetWidth, behavior: 'smooth'})
+    }
+  };
+
   return (
-    <div className="bg-black text-white relative px-5 md:px-20">
-      <h2>
+    <div
+      className="bg-black text-white relative px-5 md:px-20"
+      onMouseEnter={() => setShowArrows(true)}
+      onMouseLeave={() => setShowArrows(false)}
+    >
+      <h2 className="mb-4 text-2xl font-bold">
         {formattedCategoryName} {formattedContentType}{" "}
       </h2>
 
-      <div className="flex space-x-4 overflow-x-scroll">
+      <div className="flex space-x-4 overflow-x-scroll" ref={sliderRef}>
         {content?.map((item) => (
           <Link
             to={`/watch/${item.id}`}
@@ -48,6 +67,24 @@ const MovieSlider = ({ category }) => {
           </Link>
         ))}
       </div>
+
+      {showArrows && (
+        <>
+          <button
+            className="absolute top-1/2 -translate-y-1/2 left-5 md:left-22 flex items-center justify-center size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10"
+            onClick={scrollLeft}
+          >
+            <ChevronLeft size={24} />
+          </button>
+
+          <button
+            className="absolute top-1/2 -translate-y-1/2 right-5 md:right-22 flex items-center justify-center size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10"
+            onClick={scrollRight}
+          >
+            <ChevronRight size={24} />
+          </button>
+        </>
+      )}
     </div>
   );
 };
