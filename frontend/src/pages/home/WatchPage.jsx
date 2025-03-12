af06842b1468d12ebe602bfa85b2075b342342eb
chamila senaratne <chamilasenaratne04@gmail.com>
2025-03-12 16:57:11 +0530

    watch page

--- frontend/src/pages/home/WatchPage.jsx
+++ frontend/src/pages/home/WatchPage.jsx
@@ -1,10 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import { useContentStore } from "../../store/content";
 import axios from "axios";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Navbar from "../../components/Navbar";
-import ReactPlayer from"react-player";
+import ReactPlayer from "react-player";
+import {
+  ORIGINAL_IMG_BASE_URL,
+  SMALL_IMG_BASE_URL,
+} from "../../utils/constants";
+import { formatReleaseDate } from "../../utils/dateFunction";
+import WatchPageSkeleton from "../../components/skeletons/WatchPageSkeleton";
 
 const WatchPage = () => {
   const { id } = useParams();
@@ -14,6 +20,8 @@ const WatchPage = () => {
   const [content, setContent] = useState({});
   const [similarContent, setSimilarContent] = useState([]);
   const { contentType } = useContentStore();
+
+  const sliderRef = useRef(null);
 
   useEffect(() => {
     const getTrailers = async () => {
@@ -69,6 +77,29 @@ const WatchPage = () => {
     if (currentTrailerIdx > 0) setCurrentTrailerIdx(currentTrailerIdx - 1);
   };
 
+  const scrollLeft = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({
+        left: -sliderRef.current.offsetWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+  const scrollRight = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({
+        left: sliderRef.current.offsetWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  if(loading) return (
+    <div className="min-h-screen bg-black p-10">
+        <WatchPageSkeleton />
+    </div>
+  )
+
   return (
     <div className="bg-black min-h-screen text-white">
       <div className="mx-auto container px-4 py-8 h-full">
@@ -101,18 +132,87 @@ const WatchPage = () => {
         )}
 
         <div className="aspect-video mb-8 p-2 sm:px-10 md:px-32">
-            {trailers.length > 0 && (
-                <ReactPlayer controls={true}
-                width={"100%"} height={"70vh"} className="mx-auto overflow-hidden rounded-lg" url={`https://www.youtube.com/watch?v=${trailers[currentTrailerIdx].key}`} />
-            )}
+          {trailers.length > 0 && (
+            <ReactPlayer
+              controls={true}
+              width={"100%"}
+              height={"70vh"}
+              className="mx-auto overflow-hidden rounded-lg"
+              url={`https://www.youtube.com/watch?v=${trailers[currentTrailerIdx].key}`}
+            />
+          )}
 
-            {trailers?.length === 0 && (
-                <h2 className="text-x1 text-center mt-5">
-                    No trailers available for{" "}
-                    <span className="font-bold text-red-600">{content?.title || content?.name}</span>
-                </h2>
-            )}
+          {trailers?.length === 0 && (
+            <h2 className="text-x1 text-center mt-5">
+              No trailers available for{" "}
+              <span className="font-bold text-red-600">
+                {content?.title || content?.name}
+              </span>
+            </h2>
+          )}
         </div>
+
+        {/* movie details */}
+        <div className="flex flex-col md:flex-row items-center justify-between gap-20 max-w-6xl mx-auto">
+          <div className="mb-4 md:mb-0">
+            <h2 className="text-5xl font-bold text-balance">
+              {content?.title || content?.name}
+            </h2>
+            <p className="mt-2 text-lg">
+              {formatReleaseDate(
+                content?.release_date || content?.first_air_date
+              )}{" "}
+              |{" "}
+              {content?.adult ? (
+                <span className="text-red-600">18+</span>
+              ) : (
+                <span className="text-green-600">PG-13</span>
+              )}{" "}
+            </p>
+            <p className="mt-4 text-lg">{content?.overview}</p>
+          </div>
+          <img
+            src={ORIGINAL_IMG_BASE_URL + content?.poster_path}
+            alt="poster image"
+            className="max-h-[600px] rounded-md"
+          />
+        </div>
+
+        {similarContent.length > 0 && (
+          <div className="mt-12 max-w-5xl mx-auto relative">
+            <h3 className="text-3xl font-bold mb-4">Similar Movies/Tv Shows</h3>
+            <div
+              className="flex overflow-x-scroll no-scrollbar gap-4 pb-4 group"
+              ref={sliderRef}
+            >
+              {similarContent.map((content) => (
+                <Link
+                  key={content.id}
+                  to={`/watch/${content.id}`}
+                  className="w-52 flex-none"
+                >
+                  <img
+                    src={SMALL_IMG_BASE_URL + content?.poster_path}
+                    alt="poster_path"
+                    className="w-full h-auto rounded-md"
+                  />
+                  <h4 className="mt-2 text-lg font-semibold">
+                    {content?.title || content?.name}
+                  </h4>
+                </Link>
+              ))}
+
+              <ChevronRight
+                className="absolute top-1/2 -translate-y-1/2 right-2 w-8 h-8 opacity-0 group-hover:opacity-100 transition-all duration-300 cursor-pointer bg-red-600 text-white rounded-full"
+                onClick={scrollRight}
+              />
+              <ChevronLeft
+                className="absolute top-1/2 -translate-y-1/2 left-2 w-8 h-8 opacity-0 group-hover:opacity-100 transition-all duration-300 cursor-pointer bg-red-600 text-white rounded-full"
+                onClick={scrollLeft}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
