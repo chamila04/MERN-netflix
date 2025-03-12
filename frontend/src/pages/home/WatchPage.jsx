f0e7f00ebe414041a450a61a305229339d58a5bd
chamila senaratne <chamilasenaratne04@gmail.com>
2025-03-12 17:16:26 +0530

    Update WatchPage.jsx

--- frontend/src/pages/home/WatchPage.jsx
+++ frontend/src/pages/home/WatchPage.jsx
@@ -94,11 +94,27 @@ const WatchPage = () => {
     }
   };
 
-  if(loading) return (
-    <div className="min-h-screen bg-black p-10">
+  if (loading)
+    return (
+      <div className="min-h-screen bg-black p-10">
         <WatchPageSkeleton />
-    </div>
-  )
+      </div>
+    );
+
+  if (!content) {
+    return (
+      <div className="bg-black text-white h-screen">
+        <div className="max-w-6xl mx-auto">
+          <Navbar />
+          <div className="text-center mx-auto px-4 py-8 h-full mt-40">
+            <h2 className="text-2xl sm:text-5xl font-bold text-balance">
+              Content not found !
+            </h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="bg-black min-h-screen text-white">
@@ -185,22 +201,25 @@ const WatchPage = () => {
               className="flex overflow-x-scroll no-scrollbar gap-4 pb-4 group"
               ref={sliderRef}
             >
-              {similarContent.map((content) => (
-                <Link
-                  key={content.id}
-                  to={`/watch/${content.id}`}
-                  className="w-52 flex-none"
-                >
-                  <img
-                    src={SMALL_IMG_BASE_URL + content?.poster_path}
-                    alt="poster_path"
-                    className="w-full h-auto rounded-md"
-                  />
-                  <h4 className="mt-2 text-lg font-semibold">
-                    {content?.title || content?.name}
-                  </h4>
-                </Link>
-              ))}
+              {similarContent.map((content) => {
+                if (content.poster_path === null) return null;
+                return (
+                  <Link
+                    key={content.id}
+                    to={`/watch/${content.id}`}
+                    className="w-52 flex-none"
+                  >
+                    <img
+                      src={SMALL_IMG_BASE_URL + content?.poster_path}
+                      alt="poster_path"
+                      className="w-full h-auto rounded-md"
+                    />
+                    <h4 className="mt-2 text-lg font-semibold">
+                      {content?.title || content?.name}
+                    </h4>
+                  </Link>
+                );
+              })}
 
               <ChevronRight
                 className="absolute top-1/2 -translate-y-1/2 right-2 w-8 h-8 opacity-0 group-hover:opacity-100 transition-all duration-300 cursor-pointer bg-red-600 text-white rounded-full"
